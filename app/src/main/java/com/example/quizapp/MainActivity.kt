d65f491b16e53372ea2be19f7ded3ea0638d1470
nowsher <alinowsher@yahoo.com>
2022-10-10 00:54:27 -0500

    v1

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.quizapp
+
+import android.app.ActionBar.LayoutParams
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.allViews
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ///////////////////////////////////
+
+
+
+
+
+    }
+}
